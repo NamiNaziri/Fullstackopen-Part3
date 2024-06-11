97ba589679a825dcc447fc5ac0018235481f64bc
Nami <naminaziri1@gmail.com>
2024-06-11 20:10:07 +0300

    added fronend and change port in backend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
@@ -95,7 +98,7 @@ let persons = [
     response.json(person)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
